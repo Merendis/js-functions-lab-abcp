7f9d7014363c66911d1198467728616ed48334d8
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-17 02:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
+
 happyHolidaysTo('you')
 
 function happyHolidayTo(holiday,name){
-return "Happy ${holiday}, ${name}!"
+return `Happy ${holiday}, ${name}!`
 }
 
+happyHolidayTo(`Independence Day`)
 function holidayCountdownwn(holiday,days){
   return "It's ${days} days until ${holiday}";
 }
