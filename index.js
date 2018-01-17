f1a4405a00e2736a07d3ddab862f615771894c08
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-17 02:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ return `Happy ${holiday}, ${name}!`
 happyHolidayTo(`Independence Day`,`you`)
 
 function holidayCountdown(holiday,days){
-  return `It's ${days} days until ${holiday}!';
+  return `It's ${days} days until ${holiday}!";
 }
 
 holidayCountdown("Mother's Day",'20')
